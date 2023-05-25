725f604a3c3b556db94ee0621dfd98e46100c51d
Victor Smaniotto <victorsmaniotto@hotmail.com>
2023-05-25 19:50:18 -0300

    adicionada a correção na convesão da data, agora em dt

--- bin/consultatempo.dart
+++ bin/consultatempo.dart
@@ -4,6 +4,7 @@ import 'package:intl/intl.dart' as intl;
 final dio = Dio();
 
 double kelvinToCelsius(double kelvin) {
+  //não usar caso tenha o parametro units=metric
   double celsius = kelvin - 273.15;
   return celsius;
 }
@@ -25,13 +26,15 @@ Future main() async {
   final apikey = 'bed7d182ed69025a97968f0ea4797309';
   Future consultaTempo(String cidade) async {
     Response response = await dio.get(
-        'https://api.openweathermap.org/data/2.5/weather?q=$cidade&appid=$apikey&lang=pt_br');
+        'https://api.openweathermap.org/data/2.5/weather?q=$cidade&appid=$apikey&lang=pt_br&units=metric');
     if (response.statusCode == 200) {
-      final dataAtual =
-          now.add(Duration(milliseconds: response.data['timezone'])).toString();
+      // final dataAtual =
+      //     now.add(Duration(milliseconds: response.data['timezone'])).toString();
+      final DateTime data =
+          DateTime.fromMillisecondsSinceEpoch(response.data['dt'] * 1000);
 
       print(
-          'Temperatura de ${kelvinToCelsius(response.data['main']['temp']).toStringAsFixed(2)} C° com ${response.data['weather'][0]['description']} em ${response.data['name']} no dia ${formataData(dataAtual, isData: true)} às ${formataData(dataAtual, isData: false)}');
+          'Temperatura de ${response.data['main']['temp'].toStringAsFixed(2)} C° com ${response.data['weather'][0]['description']} em ${response.data['name']} no dia ${formataData(data.toString(), isData: true)} às ${formataData(data.toString(), isData: false)}');
     } else {
       print('Algo deu errado');
     }
